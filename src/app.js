e1832f130756261c306678c3af6b2ae782bcc5e3
Varad Kakatkar <varadkakatkar@gmail.com>
2025-11-07 17:47:00 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -1,13 +1,34 @@
 const express = require("express");
 const app = express();
 
-app.use("/hello", (req, res) => {
-  res.send("Hello from server ............");
-});
 
-app.use("/test", (req, res) => {
-  res.send("Hello World test");
-});
+
+// app.use("/hello", (req, res) => {
+//   res.send("Hello from server ............");
+// });
+
+// app.use("/test", (req, res) => {
+//   res.send("Hello World test");
+// });
+
+// app.use("/", (req, res) => {
+//   res.send("Namaste .........");
+// });
+
+
+
+app.use(
+    "/user",
+    (req,res,next)=>{
+        console.log(" Handling route user  ");
+        // res.send("Response !!!!!!");
+        next();
+    },
+    (req,res)=>{
+        console.log(" Handling route user  ");
+        res.send("Response 2 !!!!!!");
+    }
+);
 
 app.listen(3000, () => {
   console.log("Server is running on 3000");
