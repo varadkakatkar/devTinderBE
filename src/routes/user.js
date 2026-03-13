ce2be7b5088d794f48248d80f2c31e1d15fcffe7
Varad Kakatkar <varadkakatkar@gmail.com>
2026-03-13 16:12:12 +0530

    feed API

--- src/routes/user.js
+++ src/routes/user.js
@@ -22,41 +22,88 @@ userRouter.get("/user/requests/received", verifyToken, async (req, res) => {
   }
 });
 
-
 userRouter.get("/user/connections", verifyToken, async (req, res) => {
-    try {
-        const loggedInUser = req.user;
+  try {
+    const loggedInUser = req.user;
 
-        const connections = await ConnectionRequestModel.find({
-            $or: [
-                { fromUserId: loggedInUser._id, status: "accepted" },
-                { toUserId: loggedInUser._id, status: "accepted" },
-            ],
-        }).populate("fromUserId", USER_SAFE_DATA).populate("toUserId", USER_SAFE_DATA);
+    const connections = await ConnectionRequestModel.find({
+      $or: [
+        { fromUserId: loggedInUser._id, status: "accepted" },
+        { toUserId: loggedInUser._id, status: "accepted" },
+      ],
+    })
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
 
-        const data = connections.map(connection => {
-            const otherUser = connection.fromUserId._id.equals(loggedInUser._id)
-                ? connection.toUserId
-                : connection.fromUserId;
-            return {
-                _id: otherUser._id,
-                firstName: otherUser.firstName,
-                lastName: otherUser.lastName,
-                photoUrl: otherUser.photoUrl,
-                emailId: otherUser.emailId,
-                about: otherUser.about,
-                skills: otherUser.skills,
-                age: otherUser.age,
-                gender: otherUser.gender,
-            };
-        });
-        res.status(200).json({ message: "Connections fetched successfully", data: data });
-
-        
-    } catch (error) {
-        res.status(500).json({ message: "Internal server error", error: error.message });
-    }
+    const data = connections.map((connection) => {
+      const otherUser = connection.fromUserId._id.equals(loggedInUser._id)
+        ? connection.toUserId
+        : connection.fromUserId;
+      return {
+        _id: otherUser._id,
+        firstName: otherUser.firstName,
+        lastName: otherUser.lastName,
+        photoUrl: otherUser.photoUrl,
+        emailId: otherUser.emailId,
+        about: otherUser.about,
+        skills: otherUser.skills,
+        age: otherUser.age,
+        gender: otherUser.gender,
+      };
+    });
+    res
+      .status(200)
+      .json({ message: "Connections fetched successfully", data: data });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
 });
 
+userRouter.get("/feed", verifyToken, async (req, res) => {
+  try {
+    // User should see all user cards except
+    // 0. His own profile
+    // 1. His connections
+    // 2. Ignored Users
+    // 3. Already sent connection requests
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+
+    
+    if(limit > 50){
+      limit = 50;
+    }
+
+    const skip = (page - 1) * limit;
+    const loggedInUser = req.user;
+
+    // connection requests (sent + received );
+    const connections = await ConnectionRequestModel.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+
+    const hideUsersFromFeed = new Set();
+
+    connections.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
+
+    const users = await UserModel.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    }).select(USER_SAFE_DATA).skip(skip).limit(limit);
+
+    res.status(200).json({ message: "Users fetched successfully", data: users });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+});
 
 module.exports = userRouter;
